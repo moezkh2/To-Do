fc51c7881ac9ac7ef6701c8eb1e2408740cfe2d2
moezkh <khorchani_moez_eddine@outlook.com>
2023-05-19 23:28:27 +0100

    reset states

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-app.get("/", (req, res) => {
-    res.send("it is works")
-})
-app.listen(process.env.PORT, (err) => {
-    err ? console.log(err) : console.log("server is runnig in the port 3000")
-})
